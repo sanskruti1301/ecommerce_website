e1b35b82724f6d3bf366bcb3ea6e3921622f8ebf
unknown <sanskruti1301@gmail.com>
2024-05-30 13:13:20 +0530

    first commit

---
+++ ecommerce-website-php/includes/db.php
@@ -0,0 +1,5 @@
+<?php
+
+$con = mysqli_connect("localhost:8111","root","","ecom_store");
+
+?>
---
+++ ecommerce-website-php/includes/sidebar.php
@@ -0,0 +1,18 @@
+<div class="sidebar-widget">
+    
+    <div class="image-container">
+        <img src="images/model.jpeg" width="560" height="800" alt="Featured Image">
+    </div>
+</div>
+<div class="sidebar-widget">
+   
+    <div class="image-container">
+        <img src="images/model2.jpeg" width="560" height="800" alt="Featured Image">
+    </div>
+</div>
+<div class="sidebar-widget">
+   
+    <div class="image-container">
+        <img src="images/model3.jpeg" width="560" height="800" alt="Featured Image">
+    </div>
+</div>
